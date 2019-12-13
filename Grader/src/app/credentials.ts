d6fd883bf3c7c021e63bd3184d4c2d8de108658c
narit789 <narit789@Mickys-MacBook-Pro.local>
2019-12-13 22:00:21 +0700

    succesfully connecting with Firebase

---
+++ Grader/src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAHLI95qSL41RFwkwPE64epzrwf_bh6Zus",
+    authDomain: "grader-4ed87.firebaseapp.com",
+    databaseURL: "https://grader-4ed87.firebaseio.com",
+    projectId: "grader-4ed87",
+    storageBucket: "grader-4ed87.appspot.com",
+    messagingSenderId: "1069805949348",
+    appId: "1:1069805949348:web:f0d495e5ba1675aebaf131"
+  };
