1303d66e94ab3fa123a2e624097615cff41f385b
narit789 <narit789@Mickys-MacBook-Pro.local>
2019-12-14 16:22:25 +0700

    apply condition to GPAX and credit according to majors

--- Grader/src/app/pages/login/login.page.ts
+++ Grader/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/firestore';
+import { thistle } from 'color-name';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -13,7 +14,7 @@ export class LoginPage implements OnInit {
   gpax: number = 0.0
   credit: number = 0.0
   year
-
+  major
   constructor(
     public afstore: AngularFirestore
     ) { }
@@ -21,10 +22,45 @@ export class LoginPage implements OnInit {
 
   async test() {
 
+    if(this.gpax > 4 || this.gpax <0) {
+      return null;
+    }
+
+    if(this.major == "ice") {
+      if(this.credit > 146 || this.credit < 0)
+        return null;
+    }
+
+    else if(this.major == "adme") {
+      if(this.credit > 147 || this.credit < 0)
+        return null;
+    }
+
+    else if(this.major == "aero") {
+      if(this.credit > 146 || this.credit < 0)
+        return null;
+    }
+
+    else if(this.major == "nanob") {
+      if(this.credit > 147 || this.credit < 0)
+        return null;
+    }
+
+    else if(this.major == "nanom") {
+      if(this.credit > 147 || this.credit < 0)
+        return null;
+    }
+
+    else if(this.major == "ai") {
+      if(this.credit > 135 || this.credit < 0)
+        return null;
+    }
+
     this.afstore.doc<any>('userProfile/'+this.username).set({
       gpax: this.gpax,
       credit: this.credit,
       year: this.year,
+      major: this.major,
 
    
     })
