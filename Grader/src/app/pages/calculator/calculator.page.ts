d233c427596133ed8cf147505258ae3bb9189eb4
narit789 <narit789@Mickys-MacBook-Pro.local>
2019-12-15 03:50:18 +0700

    only allow stored student id in calculator page

--- Grader/src/app/pages/calculator/calculator.page.ts
+++ Grader/src/app/pages/calculator/calculator.page.ts
@@ -12,55 +12,53 @@ import { LoginPage } from '../login/login.page';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
-
+  doc: any;
+  fillall:boolean = false;
   constructor(public afstore: AngularFirestore,
     public alertController: AlertController) { }
     username
     gpax
-    fillall:boolean = false;
+    
   ngOnInit() {
   }
 
   async fetch() {
-
-    if(this.username != "" && this.gpax != null) {
-    
-      this.fillall = true;
-    }
 
     if(this.username == "" || this.gpax == null) {
       this.presentAlertVOID();
       return null;
     }
 
-  }
+    else {
 
-  
+      firebase.firestore().collection("userProfile").doc(this.username).get().then((doc)  => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          this.fillall = true;
+        } else {
+          
+          console.log("No such document!");
+          this.presentAlertUsername();
+          
 
-  async getdatafromid() {
-
-    
-
-    if(this.fillall == false) {
-      this.presentAlertfetch();
-    }
-
-    
-    
-
-
-    firebase.firestore().collection("userProfile").doc(this.username)
-    .get()
-    .then(function(doc) {
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-    } else {
-      
-      console.log("No such document!");
     }
   }).catch(function(error) {
     console.log("Error getting document:", error);
   });
+      
+    }
+
+    
+  }
+
+  
+  async getdatafromid() {
+
+    if(this.fillall == false) {
+      this.presentAlertfetch();
+    }
+    
+
   }
 
   async presentAlertVOID() {
@@ -85,4 +83,15 @@ export class CalculatorPage implements OnInit {
     await alert.present();
   }
 
+  async presentAlertUsername() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: "Student ID does not exist",
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+  
 }
